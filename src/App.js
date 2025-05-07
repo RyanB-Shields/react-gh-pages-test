5b5291c08436a53d266e5e2379c48be920415cd0
RyanB-Shields <ryan.boughton-shields@worldline.com>
2025-05-07 10:13:06 +0100

    updated MyCalendar component to show 1 month

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-// import { useState } from 'react';
-// import Calendar from 'react-calendar';
 import MyCalendar from './myCalendar.js';
-// import 'react-calendar/dist/Calendar.css';
 import './App.css';
 import wlLogo from "./images/worldline-mint-horizontal.png";
 
 function App() {
-  // const [date, setDate] = useState(new Date(2025, 3, 1));
   return (
     <div className='app'>
       <img src={wlLogo} alt='Worldline Logo'className='logo'/>
       <h1 className='title'>RDG Accounting Periods Calendar</h1>
-      <div className='calendar-container'>
         <MyCalendar />
-      </div>
     </div>
   );
 }
--- src/myCalendar.js
+++ src/myCalendar.js
@@ -1,7 +1,41 @@
-function MyCalendar() {
-  return (
-        <h1> Custom Calendar Component Test</h1>
-  );
-}
+import React, { Component } from 'react';
+import CalendarDays from './calendarDays'; 
+import './calendar.css';
 
-export default MyCalendar;
+export default class Calendar extends Component {
+  constructor() {
+    super();
+
+    this.weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    this.months = ['January', 'February', 'March', 'April', 'May', 'June', 
+                   'July', 'August', 'September', 'October', 'November', 'December'];
+
+    this.state = {
+      currentDay: new Date()
+    }
+  }
+
+  changeCurrentDay = (day) => {
+    this.setState({ currentDay: new Date(day.year, day.month, day.number) });
+  }
+  
+  render() {
+    return (
+      <div className="calendar">
+        <div className="calendar-header">
+          <h2>{this.months[this.state.currentDay.getMonth()]} {this.state.currentDay.getFullYear()}</h2>
+        </div>
+        <div className="calendar-body">
+          <div className="table-header">
+            {
+            this.weekdays.map((weekday) => {
+              return <div className="weekday"><p>{weekday}</p></div>
+            })
+          }
+          </div>
+          <CalendarDays day={this.state.currentDay} changeCurrentDay={this.changeCurrentDay}/>
+        </div>
+      </div>
+    )
+  }
+}
