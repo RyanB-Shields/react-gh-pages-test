82b2753c26c7ab9c1ce9f9b976d79d4e44335d50
RyanB-Shields <ryan.boughton-shields@worldline.com>
2025-05-22 14:40:55 +0100

    updated end of 1st period calulations / fixed calendar border styling

--- src/periods.js
+++ src/periods.js
@@ -3,29 +3,32 @@ function Periods(props) {
   // Note: Months are 0-indexed in JavaScript
   // April is month 3, March is month 2
 
-  const firstDayOfApril = new Date(props.day.getFullYear(), 3, 1);
-  // const lastDayofApril = new Date(props.day.getFullYear(), 3, 30); 
-  const lastDayOfMarch = new Date(props.day.getFullYear() +1, 2, 31);
+  //First period starts on 1st April
+  const firstPeriodStart = new Date(props.day.getFullYear(), 3, 1);
+
+  //Fist period end on last saturday of April
+  const firstPeriodEnd = new Date(props.day.getFullYear(), 3, 30);
+  firstPeriodEnd.setDate(firstPeriodEnd.getDate() - (firstPeriodEnd.getDay() + 1) %7);
+
+  //Last period ends on last day of March next year
+  const lastPeriodEnd= new Date(props.day.getFullYear() +1, 2, 31);
   
-  let period1Length = 25;
-  let periodStart = new Date(firstDayOfApril);
+  // Empty varaibles to hold the start and end dates of periods
+  let periodStart = new Date();
   let periodEnd = new Date();
   let periods = [];
 
-  
-
-  //for loop to create period start and end dates
+  //for loop to create period start and end dates and push them to the periods array
   for (let period = 1; period <= 13; period++) {
    
     if (period === 1) {
-      periodStart = new Date(firstDayOfApril);
-      //should change depending on day of the week can't end on sunday?
-      periodEnd = new Date(periodStart);
-      periodEnd.setDate(periodEnd.getDate() + period1Length);
+      periodStart = new Date(firstPeriodStart);
+      //should change depenpidng on last saturday of April
+      periodEnd = new Date(firstPeriodEnd);
     } else if (period === 13) {
       periodStart = new Date(periodEnd);
       periodStart.setDate(periodStart.getDate() + 1);
-      periodEnd = new Date(lastDayOfMarch);
+      periodEnd = new Date(lastPeriodEnd);
     } else {
       periodStart = new Date(periodEnd);
       periodStart.setDate(periodStart.getDate() + 1);
